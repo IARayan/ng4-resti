4a2d66d3fbb5b45e19f1d69fc6c7a0aefcf2ee43
Islam Al-Rayan <iarayan@live.com>
2017-05-05 05:05:51 +0200

    Additional Http methods support, separated files.

--- README.md
+++ README.md
@@ -1,2 +1,83 @@
 # ng4-resti
-Angular4 HTTP calls linguistic class.
+
+## Installation
+
+To install this library, run:
+
+```bash
+$ npm install ng4-resti --save
+```
+
+## Setup
+
+Add the RestiService to the list of the providers of your AngularModule
+```typescript
+// Import RestiService
+import { RestiService } from 'ng4-resti';
+
+@NgModule({
+  ...
+  // Specify RestiService as a provider
+  providers: [RestiService],
+  ...
+})
+export class AppModule { }
+```
+## Usage
+
+Using in a service
+
+```typescript
+import {RestiService} from 'ng4-resti';
+@Injectable()
+export class ApiService {
+  constructor(private api: RestiService) {
+  }
+ }
+```
+
+Sending a GET request
+```typescript
+getUsers(){
+    this.api.get('/users')  // request URL is /users
+    .send()                 // sends the request
+    .subscribe(res=>console.log(res)); // subscribe to the result
+}
+```
+Sending a GET request (extended example)
+```typescript
+getUsers(){
+    this.api.get('/users')      // request URL is /users
+    .query('name','islam')      // URL becomes /users?name=islam
+    .query('gender','male')     // URL becomes /users?name=islam&gender=male
+    .segment('/search')         // URL becomes /users/search?name=islam&gender=male
+    .header('Authorization','Bearer ...')   // sets a header
+    .callback(res=>console.log(res)) // callback when the request returns
+    .send()                     //sends the request, no need to subscribe to the result cause we specified a callback
+}
+```
+
+Sending a POST request
+```typescript
+getUsers(){
+    this.api.post('/users')  // request URL is /users
+    .send()                 // sends the request
+    .subscribe(res=>console.log(res)); // subscribe to the result
+}
+```
+Sending a POST request (extended example)
+```typescript
+getUsers(){
+    // request URL is /users, the body is set to the provided object
+    this.api.post('/users', {name:'islam',gender:'male'})
+    .body({'age':'27'})      // body is now merged with the {age:'27'}
+    .segment('/create')         // URL becomes /users/create
+    .header('Authorization','Bearer ...')   // sets a header
+    .callback(res=>console.log(res)) // callback when the request returns
+    .send()                     //sends the request, no need to subscribe to the result cause we specified a callback
+}
+```
+
+## License
+
+MIT © [Islam Al-Rayan](mailto:iarayan@live.com)
---
+++ src/resti.call.ts
@@ -0,0 +1,193 @@
+import {RequestMethod, RequestOptions, Http, Response, Headers} from '@angular/http';
+import {Observable} from 'rxjs/Observable';
+import {Subscription} from 'rxjs/Subscription';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+
+/**
+ * Represents an HTTP Request, using the underlying Http from @angular/http
+ */
+export class RestiCall {
+    private _url: string;
+    private _queries: { name: string, value: string }[] = [];
+    private _segments: string[] = [];
+    private _headers: { name: string, value: string }[] = [];
+    private _method: RequestMethod;
+    private _body: any = {};
+    private _http: Http;
+    private _callback: (res: Response) => void;
+    private _options: RequestOptions;
+    private _transform: (res: Response) => any;
+
+    constructor(options: { url: string, http: Http, method: RequestMethod }) {
+        if (options) {
+            this._url = options.url;
+            this._http = options.http;
+            this._method = options.method;
+        }
+    }
+
+    /**
+     *  Sets the URL to be called in this request
+     * @param {string} url - Url to be called, if a baseUrl is specified in the configuration this will be added after the baseUrl
+     * @returns {RestiCall}
+     */
+    url(url: string) {
+        this._url = url;
+        return this;
+    }
+
+    /**
+     * Adds a query parameter to the URL of the request
+     * @param name - Name of the query parameter
+     * @param value - Value of the query parameter
+     * @returns {RestiCall}
+     */
+    query(name: string, value: string) {
+        this._queries.push({name: name, value: value});
+        return this;
+    }
+
+    /**
+     * Sets the request method
+     * @param method - HTTP Method to be used can be any of the RequestMethod enum
+     * @returns {RestiCall}
+     * @see RequestMethod
+     */
+    method(method: RequestMethod): RestiCall {
+        this._method = method;
+        return this;
+    }
+
+    /**
+     * Adds a segment to request URI
+     * @param path - Path to be added to the URL
+     * @returns {RestiCall}
+     */
+    segment(path: string): RestiCall {
+        this._segments.push(path);
+        return this;
+    }
+
+    /**
+     * Adds a header to the request
+     * @param name - Name of the header
+     * @param value - Value of the header
+     * @returns {RestiCall}
+     */
+    header(name: string, value: string): RestiCall {
+        this._headers.push({name: name, value: value});
+        return this;
+    }
+
+    /**
+     * Adds data to the request body.
+     * @param body - object to be added
+     * @returns {RestiCall}
+     */
+    body(body: any): RestiCall {
+        this._body = Object.assign(this._body, body);
+        return this;
+    }
+
+    /**
+     * Sets the underlying Http Client
+     * @param http - Http Client, typically injected by Angular2/4 DI
+     * @returns {RestiCall}
+     * @see Http
+     */
+    http(http: Http): RestiCall {
+        this._http = http;
+        return this;
+    }
+
+    /**
+     * Sets the callback function that consumes the response.
+     * @param {(result)=>void} callback - Callback function to handle the response
+     * @returns {RestiCall}
+     * @example
+     * let resti = new RestiCall({ url: '/path.json', this.http, RequestMethod.Get });
+     * resti.callback((res: Response)=>{
+     *          console.log(res.json());
+     *      }).send();
+     */
+    callback(callback: (result) => void): RestiCall {
+        this._callback = callback;
+        return this;
+    }
+
+    /**
+     * Adds a transform function that applies to the request result before the final callback.
+     * @param {(res:Response)=>any} transform - Callback function to transform the response
+     * @returns {RestiCall}
+     * @example
+     * let resti = new RestiCall({ url: '/path.json', this.http, RequestMethod.Get });
+     * resti.callback((res: Response)=>{
+     *          console.log(res.json());
+     *      }).send();
+     */
+    transform(transform: (res: Response) => any): RestiCall {
+        this._transform = transform;
+        return this;
+    }
+
+    /**
+     * Sends the request, if a callback is provided it calls it,
+     * otherwise you must subscribe to the result of this function.
+     * @return {Observable<Response>|Subscription}
+     */
+    send(): Observable<any> | Subscription {
+        // Format the url
+        this.addUrlSegments();
+        this.addUrlQueryParameters();
+        this.setOptions();
+        if (this._callback) {
+            return this._http.request(this._url, this._options).subscribe(this._callback);
+        } else {
+            return this._http.request(this._url, this._options);
+        }
+    }
+
+    private addUrlSegments() {
+        if (!this._segments.length) {
+            return;
+        }
+        if (!this._url.endsWith('/')) {
+            this._url += '/';
+        }
+        for (let i = 0; i < this._segments.length; i++) {
+            this._url += encodeURIComponent(this._segments[i]) + '/';
+        }
+        this._url = this.clearLastIndexIf(this._url, '/');
+    }
+
+    private addUrlQueryParameters() {
+        if (!this._queries.length) {
+            return;
+        }
+        this._url = this.clearLastIndexIf(this._url, '/') + '?';
+        for (let i = 0; i < this._queries.length; i++) {
+            this._url += encodeURIComponent(this._queries[i].name) + '=' + encodeURIComponent(this._queries[i].value) + '&';
+        }
+        this._url = this.clearLastIndexIf(this._url, '&');
+    }
+
+    private getHeaders(): Headers {
+        let headers: Headers = new Headers();
+        for (let i = 0; i < this._headers.length; i++) {
+            headers.append(this._headers[i].name, this._headers[i].value);
+        }
+        return headers;
+    }
+
+    private setOptions() {
+        this._options = new RequestOptions({method: this._method, headers: this.getHeaders(), body: this._body});
+    }
+
+    private clearLastIndexIf(subject: string, search: string) {
+        if (subject.endsWith(search)) {
+            return subject.substring(0, this._url.lastIndexOf(search));
+        }
+        return subject;
+    }
+}
